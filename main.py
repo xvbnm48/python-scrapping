2823a58dfd743c028840a8176f050dc7a60d2427
fariz <fariz.wisnu@bluebirdgroup.com>
2023-02-06 11:22:39 +0700

    🚀 Name with space success

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ url = 'https://jkt48.com/member/list?lang=id'
 # get data
 def get_data(url):
     r = requests.get(url)
+    print(r)
     return r.text
 
 # process data
@@ -13,40 +14,27 @@ def parse(data):
     result = []
     soup = BeautifulSoup(data, 'html.parser')
     contents = soup.find('div', attrs={'class': 'row row-all-10'})
-    members = contents.find_all('a', href=True)
-    print(members)
-    # print(result)
-    for member in members:
-        # parsing data
-        link = member['href']
-        name = member.text.strip()
-        # pic = member.find('img').text.strip()
-        # print(pic)
-        # pic = member.find('img')['src']
-        # name = " ".join(name.split())
-        # br = member.find_next_sibling('br')
-        # if br:
-        #     name = name + br.text.strip()
-        # if member.find_next_sibling('br'):
-        #     name = name + member.find_next_sibling().text.strip()
-        # print(name)
-
-        if name:
-            dataDict = {
-                'name': name,
-                # 'pic': pic,
-                'link': link,
-            }
-            result.append(dataDict)
-
-        # append data
-            result.append(dataDict)
+    img_tags = contents.find_all('img')
+    link_member = contents.find_all('a', href=True)
+    for img_tag, link_member in zip(img_tags, link_member):
+        name = img_tag['alt']
+        link = "jkt48.com" + img_tag['src']
+        link_profile = "jkt48.com" + link_member['href']
+        dataDict = {
+            'name': name,
+            'link foto': link,
+            'link-profile': link_profile,
+        }
+        result.append(dataDict)
     return result
 
 def output(datas: list):
     for i in datas:
         print(i)
-
+        # print("Name", i['name'])
+        # print("Link Foto", "https://jkt48.com", i['link foto'])
+        # print("Link Profile", "https://jkt48.com",i['link-profile'])
+        # print("\n")
 if __name__ == '__main__':
     data = get_data(url)
     finalData = parse(data)
